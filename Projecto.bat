1d8b30212fbb88dd7827d869b0899c6b4684794d
Drokath16 <drokath16gh@hotmail.com>
2021-09-15 22:21:35 -0500

    Agregamos el DTO de user

---
+++ Projecto.bat
@@ -0,0 +1,6 @@
+@echo off
+
+code D:\ProjectoSoftware2
+exit
+
+
---
+++ backendcursoajva/src/main/java/net/software/backendcursoajva/BackendcursoajvaApplication.java
@@ -0,0 +1,14 @@
+package net.software.backendcursoajva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcursoajvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcursoajvaApplication.class, args);
+		System.out.println("Probando");
+	}
+
+}
