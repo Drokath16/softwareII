ded30b36217d47fee3b4e876bede62f8dcd3808e
Drokath16 <drokath16gh@hotmail.com>
2021-09-27 23:30:18 -0500

    update code with entities

--- Projecto.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-code D:\ProjectoSoftware2
-exit
-
-
--- backendcursoajva/src/main/java/net/software/backendcursoajva/services/UserServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package net.software.backendcursoajva.services;
-
-import net.software.backendcursoajva.shared.dto.UserDTO;
-
-public interface UserServiceInterface {
-   public UserDTO createUser(UserDTO userDto);
-
-}
